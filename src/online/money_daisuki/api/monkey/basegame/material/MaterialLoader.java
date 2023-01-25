8382130d0377c81c07e9b65c109cc29d83a835dd
root <root@localhost>
2023-01-25 21:06:31 +0100

    Implemented MaterialLoader.
    Change-Id: I66f3c1b0bce82ceb565bfa323007e99e075e64cd
    Signed-off-by: root <root@localhost>

--- src/online/money_daisuki/api/monkey/basegame/material/MaterialLoader.java
+++ src/online/money_daisuki/api/monkey/basegame/material/MaterialLoader.java
@@ -1,13 +1,21 @@
 package online.money_daisuki.api.monkey.basegame.material;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 import com.jme3.app.Application;
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
 import com.jme3.texture.Texture;
-import com.jme3.texture.Texture.WrapMode;
 
 import online.money_daisuki.api.base.DataSource;
 import online.money_daisuki.api.base.Requires;
+import online.money_daisuki.api.io.json.JsonDecoder;
+import online.money_daisuki.api.io.json.JsonElement;
+import online.money_daisuki.api.io.json.JsonList;
+import online.money_daisuki.api.io.json.JsonMap;
 
 public final class MaterialLoader implements DataSource<Material> {
 	private final String s;
@@ -20,37 +28,28 @@ public final class MaterialLoader implements DataSource<Material> {
 	}
 	@Override
 	public Material source() {
-		//TODO
+		try(final Reader r = new FileReader(new File(s))) {
+			final JsonMap main = new JsonDecoder(r).decode().asMap();
+			return(load(main));
+		} catch(final IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	private Material load(final JsonMap main) {
 		final AssetManager assetManager = app.getAssetManager();
-
-		/** 1. Create terrain material and load four textures into it. */
-		final Material mat = new Material(assetManager,
-				"Common/MatDefs/Terrain/Terrain.j3md");
-
-		/** 1.1) Add ALPHA map (for red-blue-green coded splat textures) */
-		mat.setTexture("Alpha", assetManager.loadTexture(
-				"Textures/Terrain/splat/alphamap.png"));
-
-		/** 1.2) Add GRASS texture into the red layer (Tex1). */
-		final Texture grass = assetManager.loadTexture(
-				"Textures/Terrain/splat/grass.jpg");
-		grass.setWrap(WrapMode.Repeat);
-		mat.setTexture("Tex1", grass);
-		mat.setFloat("Tex1Scale", 64f);
-
-		/** 1.3) Add DIRT texture into the green layer (Tex2) */
-		final Texture dirt = assetManager.loadTexture(
-				"Textures/Terrain/splat/dirt.jpg");
-		dirt.setWrap(WrapMode.Repeat);
-		mat.setTexture("Tex2", dirt);
-		mat.setFloat("Tex2Scale", 32f);
-
-		/** 1.4) Add ROAD texture into the blue layer (Tex3) */
-		final Texture rock = assetManager.loadTexture(
-				"Textures/Terrain/splat/road.jpg");
-		rock.setWrap(WrapMode.Repeat);
-		mat.setTexture("Tex3", rock);
-		mat.setFloat("Tex3Scale", 128f);
+		
+		final String url = main.get("url").asData().asString();
+		
+		final Material mat = new Material(assetManager, url);
+		
+		if(main.containsKey("textures")) {
+			final JsonList texList = main.get("textures").asList();
+			for(final JsonElement e:texList) {
+				final JsonMap texMap = e.asMap();
+				final Texture texture = assetManager.loadTexture(texMap.get("url").asData().asString());
+				mat.setTexture(texMap.get("name").asData().asString(), texture);
+			}
+		}
 		return(mat);
 	}
 }
