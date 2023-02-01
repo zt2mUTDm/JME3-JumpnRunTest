ce4a4ba14414fc948e242b9bf055570a9dad36ea
root <root@localhost>
2023-02-01 23:54:18 +0100

    Changed PrintCameraTransformCommand output.
    Change-Id: Ieb06b28ec4ce0e807139b2ead0a49ca4cc9523e7
    Signed-off-by: root <root@localhost>

--- src/online/money_daisuki/api/monkey/basegame/cam/PrintCameraTransformCommand.java
+++ src/online/money_daisuki/api/monkey/basegame/cam/PrintCameraTransformCommand.java
@@ -1,5 +1,7 @@
 package online.money_daisuki.api.monkey.basegame.cam;
 
+import java.io.PrintStream;
+
 import com.jme3.input.ChaseCamera;
 import com.jme3.scene.Spatial;
 
@@ -9,27 +11,32 @@ import online.money_daisuki.api.monkey.console.Command;
 
 public final class PrintCameraTransformCommand implements Command {
 	private final SetableDataSource<Spatial> player;
+	private final PrintStream out;
 	
-	public PrintCameraTransformCommand(final SetableDataSource<Spatial> player) {
+	public PrintCameraTransformCommand(final SetableDataSource<Spatial> player,
+			final PrintStream out) {
 		this.player = Requires.notNull(player, "player == null");
+		this.out = Requires.notNull(out, "out == null");
 	}
-	
 	@Override
 	public void execute(final Spatial caller, final String[] cmd, final Runnable done) {
 		Requires.notNull(caller, "caller == null");
 		Requires.containsNotNull(cmd, "cmd contains null");
 		Requires.lenEqual(cmd, 1);
 		
+		// TODO Use any spatial
 		if(!player.isSet()) {
 			return;
 		}
+		
 		final Spatial spatial = player.source();
 		final ChaseCamera cam = spatial.getControl(ChaseCamera.class);
 		if(cam != null) {
-			System.err.println(
-					"H: " + cam.getHorizontalRotation()
-					+ ", V: " + cam.getVerticalRotation()
-					+ ", Z: " + cam.getDistanceToTarget()
+			out.println(
+					"(" + cam.getHorizontalRotation() + ", "
+					+ cam.getVerticalRotation() + ", "
+					+ cam.getDistanceToTarget()
+					+ ")"
 			);
 		}
 		
