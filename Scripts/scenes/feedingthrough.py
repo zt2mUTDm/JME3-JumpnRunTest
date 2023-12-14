c28eab434f1f500b260514a625d7fb7514bb07cc
zt2mUTDm <123101409+zt2mUTDm@users.noreply.github.com>
2023-12-14 23:17:37 +0100

    Added Feeding through model.
    Change-Id: I30f0c6777cf0ae491ee6ebb19ae7627fe383a94b
    Signed-off-by: zt2mUTDm <123101409+zt2mUTDm@users.noreply.github.com>

---
+++ Scripts/scenes/feedingthrough.py
@@ -0,0 +1,9 @@
+from base import globals as glob
+from base import forms
+
+from base.actor import Actor
+
+class FeedingTrough(Actor):
+    def onInit(self):
+        #self.registerForTouchEvent("ActionRadius")
+        pass
